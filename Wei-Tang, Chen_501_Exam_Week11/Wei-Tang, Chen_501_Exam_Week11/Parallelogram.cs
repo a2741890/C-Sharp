8c889c822144c086b1225ad0b7c9e8994dfa7324
William <44138757+a2741890@users.noreply.github.com>
2018-10-23 09:05:44 +1000

    Add files via upload

---
+++ Wei-Tang, Chen_501_Exam_Week11/Wei-Tang, Chen_501_Exam_Week11/Parallelogram.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wei_Tang__Chen_501_Exam_Week11
+{
+    class Parallelogram
+    {
+        //Declare the data fields
+        private double heightOfParallelogram, widthOfParallelogram, areaOfParallelogram;
+        
+        //Default Constructor
+        public Parallelogram()
+        {
+
+        }
+        //Constructor with two local variables input needed
+        public Parallelogram(double heightOfparallelogram, double widthOfParallelogram)
+        {
+            Console.WriteLine("Hello, this is a parallelogram!");
+            heightOfparallelogram = this.heightOfParallelogram;
+            widthOfParallelogram = this.widthOfParallelogram;
+            
+        }
+        //By multiplying the height and width, we use this method to determine the area(return value) of the parallelogram
+        public double CalculateArea(double heighOfParallelogram, double widthOfparallelogram)
+        {
+            double value;
+            value = heighOfParallelogram*widthOfparallelogram;
+            return value;
+        }
+        //Override the ToString for this object
+        public override string ToString()
+        {
+            return "The height of the parallelogram is: " + heightOfParallelogram + "\nThe width of parallelogram is: " + widthOfParallelogram + "\nThe area of the Parallelogram is: " + CalculateArea(heightOfParallelogram,widthOfParallelogram);
+            
+        }
+        //Property of heightOfParallelogram variable
+        public double HeightOfParallelogram
+        {
+            get
+            {
+                return heightOfParallelogram;
+            }
+            set
+            {
+                heightOfParallelogram = value;
+            }
+            
+        }
+        ////Property of widthOfParallelogram variable
+        public double WidthOfParallelogram
+        {
+            get
+            {
+                return widthOfParallelogram;
+            }
+            set
+            {
+                widthOfParallelogram = value;
+            }
+        }
+        //Property of areaOfParallelogram variable
+        public double AreaOfParallelogram
+        {
+            get
+            {
+                return areaOfParallelogram;
+            }
+            set
+            {
+                areaOfParallelogram = value;
+            }
+        }
+
+    }
+}
---
+++ Wei-Tang, Chen_501_Exam_Week11/Wei-Tang, Chen_501_Exam_Week11/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wei_Tang__Chen_501_Exam_Week11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new object of Parallelogram class
+            Parallelogram parallelogram = new Parallelogram();
+            //Declare the data fields
+            double height, width;
+            int count = 3;
+            //Use for to include the data validations
+            for (int i=1; i<4; i++)
+            {
+                //Instruction
+                Console.WriteLine("Hello there! Here we are going to determine the area of a parallelogram! \n You have {0} times left to enter the value.", count);
+                //Enable user to enter the value of height
+                Console.WriteLine("Please Enter the height:");
+                double.TryParse(Console.ReadLine(), out height);
+                //To prevent user input the negative value
+                if (height > 0)
+                {
+                    parallelogram.HeightOfParallelogram = height;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Your input value can not be less than 0!\n");
+                    i--;
+                    continue;
+                }
+                //Enable user to enter the value of width
+                Console.WriteLine("Please Enter the width:");
+                double.TryParse(Console.ReadLine(), out width);
+                //To prevent user input the negative value
+                if (width > 0)
+                {
+                    parallelogram.WidthOfParallelogram = width;
+                }
+                else
+                {
+                    Console.WriteLine("Your input value can not be less than 0!\n");
+                    i--;
+                    continue;
+                }
+                //Calculate the area of the parallelogram and then use Property to store the outcome into the data of object parallelogram
+                
+                //TO help user to know how many time they got to enter the value
+                count--;
+                //Display the detail of the object parallelogram
+                Console.Clear();
+                Console.WriteLine("{0}\n", parallelogram);
+                
+                
+                
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
